3350f4673b78a2b21c24e14cb653724ab22a26cc
juan-patrick <juan-p.andrade@outlook.com>
2021-04-13 21:40:39 -0300

    lint: updated lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,35 @@
 module.exports = {
   env: {
-    es6: true,
+    commonjs: true,
     browser: true,
+    es6: true,
+    node: true,
   },
   extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier', 'import-helpers'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    indent: ['error', 2],
+    'no-undef': 0,
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'linebreak-style': ['error', 'unix'],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
 };
