0f4bb9e914c50f37b499d2252b9051d4c7bb1703
juan-patrick <juan-p.andrade@outlook.com>
2021-04-13 21:10:49 -0300

    lint: add prettier and eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {},
+};
