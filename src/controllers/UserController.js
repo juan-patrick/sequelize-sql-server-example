79c4598874b7e157daed48ba318f40958e5241ab
juan-patrick <juan-p.andrade@outlook.com>
2021-04-15 21:54:36 -0300

    refactor: user controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -7,9 +7,9 @@ module.exports = {
     return res.json(users);
   },
   async store(req, res) {
-    const { name, email } = req.body;
+    const { firstName, lastName, age, email } = req.body;
 
-    const user = await User.create({ name, email });
+    const user = await User.create({ firstName, lastName, age, email });
 
     return res.json(user);
   },
